284a3cdd8172f7d82df89f9dfdbbb5a62c933e4d
PeterAwudi <awudipeter20@gmail.com>
2025-05-16 20:00:44 +0000

    Added constants file

---
+++ constants/index.ts
@@ -0,0 +1,21 @@
+// constants/index.ts
+
+export const API_URL = 'https://api.example.com';
+export const API_KEY = 'YOUR_API_KEY_HERE';
+
+export const APP_NAME = 'Alx Listing App';
+export const APP_DESCRIPTION = 'A brief description of the Alx Listing App';
+
+export const UI_TEXT = {
+  header: 'Alx Listing App',
+  footer: 'Copyright 2023 Alx Listing App',
+  button: {
+    submit: 'Submit',
+    cancel: 'Cancel',
+  },
+};
+
+export const CONFIG = {
+  apiTimeout: 5000, // 5 seconds
+  maxItemsPerPage: 10,
+};
